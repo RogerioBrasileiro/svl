0ac8d03ddbcb807abdf4d3e6df144b395241f0e0
Marcelo <>
2022-08-09 21:00:50 -0300

    aula1

--- src/components/BookTable.jsx
+++ src/components/BookTable.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next'
 
 export default function BookTable() {
   return (
-    <div>BookTable</div>
+    <div>
+        <BootstrapTable keyField='id' data={ products } columns={ columns } />
+    </div>
   )
 }
